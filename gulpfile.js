fcb0a0f1829de02bd6dc5b52209d912060e2f5b7
JokerAn <divfooter@163.com>
2017-10-30 10:29:00 +0800

    gulpfile.js修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,58 @@
+// var gulp=require("gulp");
+// var browserSync=require("browser-sync");
+// var rename = require('gulp-rename');
+
+// //移动文件
+// gulp.task("movejs",function(){
+//    return gulp.src([
+//        "bower_components/jquery/dist/jquery.js",
+//        "bower_components/**/bootstrap.js",
+//        "bower_components/**/angular.js",
+//        "bower_components/**/angular-route.js"
+//    ])
+//        .pipe(rename({dirname:''}))
+//        .pipe(gulp.dest('app/js/lib'))
+// });
+// //移动文件
+// gulp.task("movecss",function(){
+//    return gulp.src([
+//        "bower_components/**/bootstrap.css"
+//    ])
+//        .pipe(rename({dirname:''}))
+//        .pipe(gulp.dest('app/css/lib'))
+// });
+//
+// gulp.task("browserSync",function(){
+//     browserSync({
+//         server:{
+//             baseDir:'app'
+//         }
+//     });
+// });
+// gulp.task("watch",['movecss','movejs','browserSync'],function(){
+//     gulp.watch("app/**/*.html",browserSync.reload);
+//     gulp.watch("app/**/*.css",browserSync.reload);
+//     gulp.watch("app/**/*.js",browserSync.reload);
+// });
+
+
 var gulp=require("gulp");
 var browserSync=require("browser-sync");
-var rename = require('gulp-rename');
-
-//移动文件
-gulp.task("movejs",function(){
-   return gulp.src([
-       "bower_components/jquery/dist/jquery.js",
-       "bower_components/**/bootstrap.js",
-       "bower_components/**/angular.js",
-       "bower_components/**/angular-route.js"
-   ])
-       .pipe(rename({dirname:''}))
-       .pipe(gulp.dest('app/js/lib'))
-});
-//移动文件
-gulp.task("movecss",function(){
-   return gulp.src([
-       "bower_components/**/bootstrap.css"
-   ])
-       .pipe(rename({dirname:''}))
-       .pipe(gulp.dest('app/css/lib'))
-});
+var historyApiFallback =require('connect-history-api-fallback');//angular 去掉url的#号
 
 gulp.task("browserSync",function(){
     browserSync({
         server:{
-            baseDir:'app'
-        }
-    });
+            baseDir:"app",
+            middleware: [ historyApiFallback() ]//
+        },
+        port: 9952,
+
+
+    })
 });
-gulp.task("watch",['movecss','movejs','browserSync'],function(){
-    gulp.watch("app/**/*.html",browserSync.reload);
+gulp.task("watch",['browserSync'],function(){
     gulp.watch("app/**/*.css",browserSync.reload);
+    gulp.watch("app/**/*.html",browserSync.reload);
     gulp.watch("app/**/*.js",browserSync.reload);
 });
